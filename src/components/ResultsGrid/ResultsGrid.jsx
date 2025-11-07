ae24a25bd9d0ad8fa5f972128bd4677c4aaf9a1d
danpgomez <6013282+danpgomez@users.noreply.github.com>
2025-11-07 16:03:46 -0500

    Add error and no results messages

--- src/components/ResultsGrid/ResultsGrid.jsx
+++ src/components/ResultsGrid/ResultsGrid.jsx
@@ -3,15 +3,19 @@ import "./ResultsGrid.css";
 
 function ResultsGrid({ searchResults, status }) {
 
-    if (status == "error") {
-        return "Oops! Something went wrong";
+    if (status == "error" || !searchResults) {
+        return <p className="error-message">Oops! Something went wrong"</p>;
+    }
+
+    if (searchResults.length === 0) {
+        return <p className="no-results-message">No results found</p>;
     }
 
     return (
        <ul className="results-grid">
         {searchResults.map(movie => {
             return <ResultCard result={movie} />;
-        })};
+        })}
        </ul>
     );
 }
